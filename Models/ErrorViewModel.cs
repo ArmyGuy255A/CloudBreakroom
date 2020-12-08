8b5189baa8cf0c5b18e7b7d842c921993e64d824
ArmyGuy255A <mrdieppa@gmail.com>
2020-12-08 14:17:55 -0500

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CloudBreakroom.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
